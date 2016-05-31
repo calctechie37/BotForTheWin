1274ab28df986b4736e5c6e6da8bc5ce2b627a23
Charles Zhang <charleszhang2000@gmail.com>
2016-05-31 12:55:50 -0400

    AI for Super Tic Tac Toe

--- SuperTicTacToe/Game.java
+++ SuperTicTacToe/Game.java
@@ -30,17 +30,17 @@ public class Game{
 
     public boolean add(int outer,int inner,String marker)
     {
-	if(pos[(outer/3)*27+(inner/3)*9+(inner%3)+(outer%3)*3]!="X"||pos[(outer/3)*27+(inner/3)*9+(inner%3)+(outer%3)*3]!="O")
+	if(pos[(outer/3)*27+(inner/3)*9+(inner%3)+(outer%3)*3]==" ")
 	    {
 		pos[(outer/3)*27+(inner/3)*9+(inner%3)+(outer%3)*3]=marker;
-		System.out.println(toString());
+		//System.out.println(toString());
 		if(checkWinInner(outer,marker))
 		    {
 			for(int i=0;i<9;i++)
 			    {
 				pos[(outer/3)*27+(i/3)*9+(i%3)+(outer%3)*3]=marker;
 			    }
-			System.out.println(toString());
+			
 		
 		    }
 		return true;
@@ -177,23 +177,195 @@ public class Game{
 	return board;
     }
     
+    /*implementing a neural network
+      I intend to have weighted percptrons and a feedback function. 
+
+      The perceptrons will test whether the current inner board can be solved when putting a marker down at a certain place 
+      and if the whole board will be solved. The feedback function will change the weights based on whether the player (you) wiins a game or an inner board.
+     */
+    public void feedback()
+    {
+	try
+	    {
+		PrintWriter weightsout = new PrintWriter(new BufferedWriter(new FileWriter("Pweights.txt")));
+		weightsout.close();
+	    }
+	catch(Exception e)
+	    {System.out.println(e);}
+    }
+
+    public int AI(int outer)
+    {
+	if(checkWinInner(outer,"X") || checkWinInner(outer,"O"))
+	    {
+		AI((int)(Math.random()*9));
+	    }
+	try{
+	    BufferedReader in = new BufferedReader(new FileReader("Pweights.txt"));
+	    String first = in.readLine();
+	    double innerWeight = Double.parseDouble(first.split(" ")[0]);
+	    double outerWeight = Double.parseDouble(first.split(" ")[1]);
+	    double innervalue = 0;
+	    double outervalue = 0;
+	    
+	    for(int i=0;i<9;i++)
+		{
+		    /*
+		    if(add(outer,i,"O"))
+			{
+			    if((innerWeight*1 + outerWeight*1)>=1)
+				{
+				    System.out.println("AI put down marker at " + (outer+1) + " " + (i+1));
+				    return i;			    
+				}
+
+			    if(!checkWinInner(outer,"0"))
+				{
+				    remove(outer,i);
+				}
+			    else
+				{
+				    if(checkWin()!="")
+					{
+					    if((innerWeight*1 + outerWeight*1)<1)
+						{
+						    remove(outer,i);
+						}
+					    else
+						{
+						    System.out.println("AI put down marker at" + (outer+1) + " " + (i+1));
+						    return i;}
+					}
+				    else
+					{
+					    if((innerWeight*1 + outerWeight*1)<1)
+						{
+						    remove(outer,i);
+						}
+					    else
+						{
+						    System.out.println("AI put down marker at" + (outer+1) + " " +(i+1));
+						    return i;
+						}
+					}
+				}
+			}
+		    */
+		    double bias = 0.5*0.5 + 0.5*0.5;
+		    if(add(outer,i,"O") && checkWinInner(outer,"O"))
+			{
+			    innervalue = 1;
+			    if(checkPossibleWin(i)=="X")
+				{
+				    innervalue = 1/2;
+				}
+			    remove(outer,i);
+			}
+		    else if(add(outer,i,"O") && !checkWinInner(outer,"O"))
+			{
+			    if(checkPossibleWin(i)=="X")
+				{
+				    innervalue = 0;
+				}
+			    else
+				{
+				    innervalue = 1/2;
+				}
+			    remove(outer,i);
+			}
+
+		    if(add(outer,i,"O") && checkWin()=="O")
+			{
+			    outervalue = 1;
+			    remove(outer,i);
+			}
+		    else if(add(outer,i,"O"))
+			{
+			    outervalue = 1/2;
+			    remove(outer,i);
+			}
+
+		    // remove(outer,i);
+		    if((outervalue*outerWeight + innervalue*innerWeight)>=bias)
+			{
+			    add(outer,i,"O");
+			    System.out.println("AI put down marker at " + (outer+1) + " " + (i+1));
+			    return i;
+			} 
+		}
+	}
+	catch(Exception e)
+	    {System.out.println(e);}  
+	return outer;
+    }
+
+    public String checkPossibleWin(int outer)
+    {
+	for(int i=0;i<9;i++)
+	    {
+		if(add(outer,i,"X") && checkWinInner(outer,"X"))
+		   {
+		       remove(outer,i);
+		       return "X";
+		   }
+		remove(outer,i);
+	    }
+	return "";
+    }
+
     public static void main(String[]args)
     {
 	String[] possibleInputs = {"1","2","3","4","5","6","7","8","9"};
 	Game test = new Game();
 	System.out.println(test);
-
+	    
+	System.out.println("You start!");
+	
+	int outer,inner;
+	System.out.println("Select Outer");	
+	outer = Integer.parseInt(test.getUserInput(possibleInputs))-1;
+	
+	System.out.println("Select Inner");	
+	inner = Integer.parseInt(test.getUserInput(possibleInputs))-1;
+	
+	test.add(outer,inner,"X");
+        System.out.println(test);	    
+		    
 	while(test.checkWin()=="")
 	    {
-		System.out.println("You start!");
-		int outer,inner;
-		System.out.println("Select Outer");	
-		outer = Integer.parseInt(test.getUserInput(possibleInputs))-1;
-		System.out.println("Select Inner");	
-		inner = Integer.parseInt(test.getUserInput(possibleInputs))-1;
-		test.add(outer,inner,"X");
-		//		System.out.println(test);	    
+		//		System.out.println(outer);
+		outer = test.AI(inner);
+		System.out.println(test);	    		
+		if(test.checkWinInner(outer,"X") || test.checkWinInner(outer,"O"))
+		    {
+			System.out.println("Select Outer");
+			outer = Integer.parseInt(test.getUserInput(possibleInputs))-1;
+
+			System.out.println("Select Inner");
+			inner = Integer.parseInt(test.getUserInput(possibleInputs))-1;
+
+			test.add(outer,inner,"X");
+			System.out.println(test);	    
+		    }
+		else
+		    {
+			System.out.println("Your turn! Outer: " + (outer+1)+ " Inner: "); 
+			inner = Integer.parseInt(test.getUserInput(possibleInputs))-1;
+			
+			test.add(outer,inner,"X");
+			System.out.println(test);	    
+		    }
 	    }
+	
+	if(test.checkWin()=="X")
+	    {
+		System.out.println("You won!!");
+	    }
+	if(test.checkWin()=="O")
+	    {
+		System.out.println("BotForWin!!");
+	    }
+
 	
 	
 	//test.add(1,4,"X");
